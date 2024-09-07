e1cef31f901efa44c2a3f078867ca465288836f6
Iggy Villamar <iggy@ivstudio.com>
2024-09-07 18:23:24 -0400

    rm index from directories

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { isAnagram } from './algorithms/anagram/anagram';
+
+console.log(isAnagram('listen', 'silent')); // true
+console.log(isAnagram('anagram', 'nagaram')); // true
+console.log(isAnagram('rails safety', 'fairy tales')); // false
