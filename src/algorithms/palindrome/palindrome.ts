52b3595f5916bb09ff97494dfff686de6cdff70e
Iggy Villamar <iggy@ivstudio.com>
2024-09-17 09:30:29 -0400

    update explanation

--- src/algorithms/palindrome/palindrome.ts
+++ src/algorithms/palindrome/palindrome.ts
@@ -4,10 +4,11 @@
     that reads the same forward and backward.
 
     Example: madam, 12321, racecar,
-*/
 
-/* -------------------------------------------------------------------------- 
-    Approach:
+    Constraint:
+    - The solution must use only constant extra space, i.e., O(1) space complexity.
+
+    Explanation:
     - Convert the input to a string
     - Initialize two pointers, left and right, pointing to the start and end of the string respectively
     - While left is less than right, compare the characters at the left and right pointers
@@ -15,19 +16,20 @@
     - Otherwise, increment left and decrement right
     - If the loop completes, return true
 
-    Time complexity:
-    - O(n), where n is the length of the input string
-
-    Space complexity:
-    - O(1)
-
     Limitation:
     - This approach does not work for negative numbers or special characters
     - To handle negative numbers, convert the number to a string and remove the '-' sign
     - To handle special characters, remove them from the string before comparison
     RegEx can be used to remove special characters and spaces
     - str.replace(/[^a-zA-Z0-9]/g, '')
---------------------------------------------------------------------------*/
+
+    Time complexity:
+    - O(n), where n is the length of the input string
+
+    Space complexity:
+    - O(1)
+*/
+
 export const isPalindrome = (input: string | number): boolean => {
     const str = input.toString().toLowerCase().split(' ').join('');
 
