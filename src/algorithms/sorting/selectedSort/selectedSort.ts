2fddcdf135f788fbc788c51765ebdf4949ba18f9
Iggy Villamar <iggy@ivstudio.com>
2024-11-12 06:33:30 -0500

    optimize DoublyLinkedList get

--- src/algorithms/sorting/selectedSort/selectedSort.ts
+++ src/algorithms/sorting/selectedSort/selectedSort.ts
@@ -1,3 +1,15 @@
+/**
+ * selectedSort
+ * Sorts an array of numbers in ascending order using the selection sort algorithm.
+ * - Finds the smallest element in the unsorted portion of the array.
+ * - Swaps it with the first unsorted element.
+ * - Repeats the process for the next unsorted element.
+ * - Continues until the entire array is sorted.
+ *
+ * @param arr - The array of numbers to be sorted.
+ * @returns The sorted array.
+ */
+
 export function selectedSort(arr: number[]): number[] {
     let lowest;
     for (let i = 0; i < arr.length; i++) {
